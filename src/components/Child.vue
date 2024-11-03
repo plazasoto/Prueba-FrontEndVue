9eafc870957638b86287e114b193ed70cbfbbbd4
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-11-03 01:24:17 -0300

    componentes Parent y Child

---
+++ src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <h3>Child</h3>
+        <input type="text" v-model="textoChild" placeholder="Ingreso de texto(ᓀ‸ᓂ)">
+        <button @click="enviarTexto" >↑ Enviar ↑</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                textoChild: "",
+            }
+        },
+        methods:{
+            enviarTexto(){
+                this.$emit('enviar-texto', this.textoChild);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container{
+        border-style: solid;
+        border-color: cyan;
+    }
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Parent</h2>
+        <p>Texto recibido → <u>{{ textoRecibido }}</u></p>
+        <Child @enviar-texto="recibirTexto"/>
+    </div>
+</template>
+
+<script>
+    import Child from './Child.vue'
+    export default{
+        components: {
+            Child
+        },
+        data(){
+            return{
+                textoRecibido:""
+            }
+        },
+        methods:{
+            recibirTexto(texto){
+                this.textoRecibido = texto;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        border-style: solid;
+        border-color: blue;
+    }
+</style>
